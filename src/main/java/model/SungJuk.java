8fef728bd88f7ce2ab2c776d05129473a4cfa145
hideonbush3 <ppoii0961@gmail.com>
2023-05-15 11:18:43 +0900

    jpa 프로젝트 초기화

---
+++ src/main/java/model/SungJuk.java
@@ -0,0 +1,10 @@
+package model;
+
+public class SungJuk {
+    private String name;
+    private int kor;
+    private int eng;
+    private int mat;
+    private double avg;
+    private String grd;
+}
