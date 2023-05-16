c0bbceda58e938b5b79acf6dca41e350ff490271
hideonbush3 <ppoii0961@gmail.com>
2023-05-16 13:04:22 +0900

    JPQL 실습 4 - Criteria API

--- src/main/java/model/Employee.java
+++ src/main/java/model/Employee.java
@@ -1,14 +1,19 @@
 package model;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 import java.math.BigDecimal;
 import java.sql.Date;
 
-@Data
+@Setter
+@Getter
 @Entity
 @Table(name = "employees")
+@NoArgsConstructor
 public class Employee {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -46,7 +51,25 @@ public class Employee {
     private Long deptid;
 
     // 조인대상 객체를 쓴다
-    @ManyToOne  // 테이블 연관관계 다대일
-    @JoinColumn(name="department_id")   // 실제 컬럼명이 아니고 department 테이블의 id와 조인
-    private Department department;
+//    @ManyToOne  // 테이블 연관관계 다대일
+//    @JoinColumn(name="department_id")   // 실제 컬럼명이 아니고 department 테이블의 id와 조인
+//    private Department department;
+
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("Employee{");
+        sb.append("empid=").append(empid);
+        sb.append(", fname='").append(fname).append('\'');
+        sb.append(", lname='").append(lname).append('\'');
+        sb.append(", email='").append(email).append('\'');
+        sb.append(", phone='").append(phone).append('\'');
+        sb.append(", hdate=").append(hdate);
+        sb.append(", jobid='").append(jobid).append('\'');
+        sb.append(", sal=").append(sal);
+        sb.append(", comm=").append(comm);
+        sb.append(", mgrid=").append(mgrid);
+        sb.append(", deptid=").append(deptid);
+        sb.append('}');
+        return sb.toString();
+    }
 }
